596992e9c0bae1979d64ef298d51e5086897d845
lukebandy <60064928+lukebandy@users.noreply.github.com>
2020-04-18 10:42:55 +0100

    Initial map generation

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,17 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
-{
+public class GameController : MonoBehaviour {
+
+    // Static variables
+    public static GameController main;
+
+    // Prefabs
+    [SerializeField]
+    private GameObject prefabTile;
+    [SerializeField]
+    private GameObject prefabFence;
+
+    // Public variables
+
+    // Private variables
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
+    }
+
+    public void GenerateMap(int sizeX = 20, int sizeY = 20, int hoses = 3) {
+        // Tiles
+        int child = 0;
+        for (int x = 0; x < sizeX; x++) {
+            for (int y = 0; y < sizeY; y++) {
+                if (child + 1 > transform.Find("Tiles").childCount)
+                    Instantiate(prefabTile, transform.Find("Tiles"));
+                transform.Find("Tiles").GetChild(child).transform.position = new Vector3(x, 0, y);
+                transform.Find("Tiles").GetChild(child).gameObject.SetActive(true);
+                child++;
+            }
+        }
+        while (child + 1 < transform.Find("Tiles").childCount) {
+            transform.Find("Tiles").GetChild(child).gameObject.SetActive(false);
+            child++;
+        }
+
+        // Fence
+        
+        for (int x = 0; x < sizeX; x++) {
+            
+        }
+        for (int y = 0; y < sizeY; y++) {
+            
+        }
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile : MonoBehaviour
-{
+public class Tile : MonoBehaviour {
+
+    public bool wet;
+
+    [SerializeField]
+    private Material materialDry;
+    [SerializeField]
+    private Material materialWet;
+
+    private MeshRenderer meshRenderer;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        meshRenderer = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (wet)
+            meshRenderer.material = materialWet;
+        else
+            meshRenderer.material = materialDry;
     }
 }
